c8591b6f848f1f694a96e0362467f04718069689
Trojan-Addon <hughesconnor011@gmail.com>
2025-11-06 22:01:32 +0000

    VersionUtil.java

--- src/main/java/com/nnpg/glazed/VersionUtil.java
+++ src/main/java/com/nnpg/glazed/VersionUtil.java
@@ -1,4 +1,4 @@
-package com.nnpg.glazed;
+package com.wazify.Trojan;
 
 import net.minecraft.SharedConstants;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -8,35 +8,35 @@ import net.minecraft.util.collection.DefaultedList;
 public class VersionUtil {
 
     public static ItemStack getArmorStack(ClientPlayerEntity player, int slot) {
-    return player.getInventory().getStack(slot);
+        return player.getInventory().getStack(slot);
     }
 
     public static ItemStack getArmorStackByType(ClientPlayerEntity player, int armorType) {
-    return player.getInventory().getStack(armorType);
+        return player.getInventory().getStack(armorType);
     }
 
     public static int getSelectedSlot(ClientPlayerEntity player) {
-    return player.getInventory().selectedSlot;
+        return player.getInventory().selectedSlot;
     }
 
     public static void setSelectedSlot(ClientPlayerEntity player, int slot) {
-    player.getInventory().setSelectedSlot(slot);
+        player.getInventory().setSelectedSlot(slot);
     }
 
     public static double getPrevX(net.minecraft.entity.Entity entity) {
-    return entity.lastRenderX;
+        return entity.lastRenderX;
     }
 
     public static double getPrevY(net.minecraft.entity.Entity entity) {
-    return entity.lastRenderY;
+        return entity.lastRenderY;
     }
 
     public static double getPrevZ(net.minecraft.entity.Entity entity) {
-    return entity.lastRenderZ;
+        return entity.lastRenderZ;
     }
 
     public static DefaultedList<ItemStack> getMainInventory(ClientPlayerEntity player) {
-    // If main is not available, you may need to loop over getStack(i) for 0-35
-    return null; // Placeholder, update as needed for your use case
-    } //whos the nigga that wrote the above?
+        // If main is not available, you may need to loop over getStack(i) for 0-35
+        return null; // Placeholder, update as needed for your use case
+    } //whos the author that wrote the above?
 }
